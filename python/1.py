3e33e4ecfdc556a1cdd2379db9db4f95839af763
zhanglun <zhanglun1410@gmail.com>
2024-11-11 10:57:52 +0800

    chore: venv

--- python/1.py
+++ python/1.py
@@ -1,8 +1,37 @@
-from web3 import Web3, HTTPProvider
+from web3 import Web3
 
-address = '0xxx'
-rpc = 'https://bsc-dataseed1.binance.org:443'
+# Your Infura Project ID
+INFURA_SECRET_KEY = '7fe353dd8591489db345b657ebe5c910'
 
-web3 = Web3(HTTPProvider(rpc))
-balance = web3.fromWei(web3.eth.getBalance(address), "ether")
-print(balance)
+# get w3 endpoint by network name
+def get_w3_by_network(network='mainnet'):
+    infura_url = f'https://{network}.infura.io/v3/{INFURA_SECRET_KEY}' # 接入 Infura 节点
+    w3 = Web3(Web3.HTTPProvider(infura_url))
+    return w3
+
+
+def main():
+    
+    # 🐳 Task 1: 接入并读取区块链信息
+
+    # 接入 Web3
+    w3 = get_w3_by_network(network='mainnet')
+
+    # 检查接入状态
+    print(w3.is_connected())
+
+    # 当前区块高度
+    print(w3.eth.block_number)
+
+    # V神 3号钱包地址
+    vb = '0x220866b1a2219f40e72f5c628b65d54268ca3a9d'
+
+    # 地址格式转换
+    address = Web3.to_checksum_address(vb)
+
+    # 查询地址 ETH余额
+    balance = w3.eth.get_balance(address) / 1e18
+    print(f'V神地址余额: {balance = } ETH')
+
+if __name__ == "__main__":
+    main()
